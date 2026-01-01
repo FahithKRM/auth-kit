e03849d75ed347c1651db6db6cd98099edf68de6
FahithKRM <krmfahith@gmail.com>
2026-01-02 00:15:03 +0530

    feat: add User model

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
